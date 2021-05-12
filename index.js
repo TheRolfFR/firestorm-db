74c7b195529f829b1333a74441b631fd397ba9a8
TheRolf <yann.lv@live.fr>
2021-05-12 22:13:39 +0200

    Fixed bugs + added id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios")
 
 /**
- * @typedef {SearchOption}
+ * @typedef {Object} SearchOption
  * @property {String} field The field you want to search in
  * @property {"!=" | "==" | ">=" | "<=" | "<" | ">" | "in" | "includes" | "startsWith" | "endsWith" | "array-contains" | "array-contains-any"} criteria // filter criteria
  * @property {String | Number | Boolean | Array } value // the value you want to compare
@@ -9,6 +9,8 @@ const { default: axios } = require("axios")
 
 let _address = undefined
 let _token = undefined
+
+const ID_FIELD_NAME = 'id'
 
 const readAddress = () => {
   if(!_address)
@@ -20,7 +22,7 @@ const writeAddress = () => {
   if(!_address)
     throw new Error('Firestorm address was not configured')
   
-  return _address + 'put.php'
+  return _address + 'post.php'
 }
 
 const writeToken = () => {
@@ -30,15 +32,40 @@ const writeToken = () => {
   return _token
 }
 
+/**
+ * Class representing a collection
+ * @template T
+ */
 class Collection {
-  /** @param {String} name The name of the Collection */
-  constructor(name) {
+  /** 
+   * @param {String} name The name of the Collection
+   * @param {Function?} addMethods Additional methods and data to add to the objects
+   */
+  constructor(name, addMethods = el => el) {
+    this.addMethods = addMethods
     this.collectionName = name
+  }
+
+  __add_methods(req) {
+    return new Promise((resolve, reject) => {
+      req
+      .then(el => {
+        if(Array.isArray(el)) {
+          return resolve(el.map(e => this.addMethods(e)))
+        }
+
+        el[Object.keys(el)[0]][ID_FIELD_NAME] = Object.keys(el)[0]
+        el = el[Object.keys(el)[0]]
+
+        // else on the object itself
+        return resolve(this.addMethods(el))
+      }).catch(err => reject(err))
+    })
   }
 
   /**
    * Auto-extracts data from Axios request
-   * @param {Promise<any>} request The Axios concerned request
+   * @param {Promise<T>} request The Axios concerned request
    */
   __extract_data(request) {
     return new Promise((resolve, reject) => {
@@ -53,26 +80,41 @@ class Collection {
   /**
    * Get an element from the collection
    * @param {String|Number} id The entry ID
-   * @returns {Promise} Result entry you may be looking for
+   * @returns {Promise<T>} Result entry you may be looking for
    */
   get(id) {
-    return this.__extract_data(axios.get(readAddress(), { data: {
+    return this.__add_methods(this.__extract_data(axios.get(readAddress(), { data: {
       "collection": this.collectionName,
       "command": "get",
       "id": id
-    }}))
+    }})))
   }
 
   /**
    * Search through collection
-   * @param {SearchOption[]} searchOptions 
+   * @param {SearchOption[]} searchOptions
+   * @returns {Promise<T[]>}
    */
   search(searchOptions) {
-    return this.__extract_data(axios.get(readAddress(), { data: {
-      "collection": this.collectionName,
-      "command": "search",
-      "search": searchOptions
-    }}))
+    return new Promise((resolve, reject) => {
+      this.__extract_data(axios.get(readAddress(), {
+        data: {
+          "collection": this.collectionName,
+          "command": "search",
+          "search": searchOptions
+        }
+      })).then(res => {
+        const arr = []
+        Object.keys(res).forEach(contribID => {
+          const tmp = res[contribID]
+          tmp[ID_FIELD_NAME] = contribID
+          arr.push(tmp)
+        })
+
+        resolve(this.__add_methods(Promise.resolve(arr)))
+
+      }).catch(err => reject(err))
+    })
   }
 
   /**
@@ -80,10 +122,18 @@ class Collection {
    * @returns {Promise} // the get promise of the collection raw file content
    */
   read_raw() {
-    return this.__extract_data(axios.get(readAddress(), { data: {
-      "collection": this.collectionName,
-      "command": "read_raw"
-    }}))
+    let data = this.__extract_data(axios.get(readAddress(), {
+      data: {
+        "collection": this.collectionName,
+        "command": "read_raw"
+      }
+    }))
+
+    Object.keys(data).forEach(key => {
+      this.addMethods(data[key])
+    })
+
+    return data
   }
 
   /**
@@ -92,13 +142,18 @@ class Collection {
    * @param {Object?} value The value for this command 
    * @returns {Object} Write data object
    */
-  __write_data(command, value = undefined) {
+  __write_data(command, value = undefined, multiple = false) {
     const obj = {
       "token": writeToken(),
       "collection": this.collectionName,
       "command": command
     }
-    if(value) obj["value"] = value
+    if(value) {
+      if(multiple)
+        obj["values"] = value
+      else
+        obj["value"] = value
+    }
 
     return obj
   }
@@ -127,7 +182,7 @@ class Collection {
    * @returns {Promise<any>}
    */
   addBulk(values) {
-    return this.__extract_data(axios.post(writeAddress(), this.__write_data('addBulk', values)))
+    return this.__extract_data(axios.post(writeAddress(), this.__write_data('addBulk', values, true)))
   }
 
   /**
@@ -144,18 +199,18 @@ class Collection {
    * @param {String[] | Number[]} keys The key from the entries to remove
    * @returns {Promise<any>}
    */
-  removeBuks(keys) {
-    return this.__extract_data(axios.post(writeAddress(), this.__write_data('write_raw', keys)))
+  removeBulk(keys) {
+    return this.__extract_data(axios.post(writeAddress(), this.__write_data('removeBulk', keys)))
   }
 
   /**
    * Sets an entry in the JSON
    * @param {String} key The key of the value you want to set
-   * @param {Object} value The value tou want for this key
+   * @param {Object} value The value you want for this key
    * @returns {Promise<any>}
    */
   set(key, value) {
-    const data = this.__write_data('write_raw', value)
+    const data = this.__write_data('set', value)
     data['key'] = key
     return this.__extract_data(axios.post(writeAddress(), data))
   }
@@ -167,9 +222,8 @@ class Collection {
    * @returns {Promise<any>}
    */
   setBulk(keys, values) {
-    const data = this.__write_data('write_raw')
+    const data = this.__write_data('setBulk', values, true)
     data['keys'] = keys
-    data['values'] = values
     return this.__extract_data(axios.post(writeAddress(), data))
   }
 }
@@ -193,11 +247,11 @@ module.exports = {
     return _token
   },
   /**
-   * 
    * @param {String} name Collection name to get
+   * @param {Function?} addMethods Additional methods and data to add to the objects
    */
-  collection: function(name) {
-    return new Collection(name)
+  collection: function(name, addMethods = el => el) {
+    return new Collection(name, addMethods)
   },
 
   /**
@@ -206,5 +260,7 @@ module.exports = {
    */
   table: function(name) {
     return this.collection(name)
-  }
+  },
+
+  ID_FIELD: ID_FIELD_NAME
 }
