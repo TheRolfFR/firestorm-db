40d93457c0e0f52a393e81cd9deefdba0c7e2478
TheRolf <yann.lv@live.fr>
2021-10-08 20:51:31 +0200

    Fixed that damn php_server_kill script

--- tests/php_server_kill.sh
+++ tests/php_server_kill.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if ! command -v ps > /dev/null 2>&1
+if ! command -v killall > /dev/null 2>&1
 then
-    echo "ps not found, server kill skipped"
+    echo "killall not found, server kill skipped"
     exit
 fi
-kill -9 $(ps ax | grep php | fgrep -v grep | awk '{ print $1 }') > /dev/null 2>&1
+killall php > /dev/null 2>&1
